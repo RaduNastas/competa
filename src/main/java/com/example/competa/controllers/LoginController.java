c1c7bcc52599cfbcd77763e3743f30f235de5b2a
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-07-02 19:57:36 +0200

    25 Jun Update - after 17:00

---
+++ src/main/java/com/example/competa/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.example.competa.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class LoginController {
+    @GetMapping("/login")
+    String login() {
+        return "login";
+    }
+}
